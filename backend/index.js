301aa64575a7737efaac4b9aadbf365723dff07a
Bryant Wong <bryanthwong@gmail.com>
2023-11-24 18:46:12 -0500

    removed comments

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,6 @@ import { Server } from 'socket.io';
 import dotenv from 'dotenv';
 import db from './db/db.connection.js';
 //seperate exports into routes/controller dir
-import Room from './models/Room.js';
-import User from './models/User.js';
 import routes from './routes/index.js';
 import { generateRoutes } from './helpers/index.js';
 
@@ -35,54 +33,7 @@ app.get('/', async (req, res) => {
   // res.sendFile(__dirname + '/index.html');
 });
 
-//seperate into routes/controller dir
-// app.get('/rooms', async (req,res)=>{
-//   try{
-//     const rooms = await Room.find();
-//     res.json(rooms).status(200)
-//   }catch(e){
-//     res.json({error:`error processing on /rooms get route ${e}`}).status(500)
-//   }
-// })
-
-app.get('/users', async (req,res)=>{
-  try{
-    const users = await User.find().populate('rooms');
-    res.json(users).status(200)
-  }catch(e){
-    res.json({error:`error processing on /users get route ${e}`}).status(500)
-  }
-})
-
 generateRoutes(routes,app)
-
-app.post('/room',async (req,res)=>{
-  try{
-    const roomsCount = await Room.countDocuments()
-    const newRoom = new Room({roomNumber:roomsCount})
-    await newRoom.save()
-    res.json(newRoom).status(200)
-  }catch(e){
-    res.json({error:`error processing on /room post route ${e}`}).status(500)
-  }
-})
-
-app.post('/user',async (req,res)=>{
-  try{
-    const username = req.body.username
-    const existingUser = await User.findOne({username})
-
-    if(existingUser) {
-      return res.json({error:`user with ${username} already exist`}).status(400)
-    }
-
-    const newUser = new User({username})
-    await newUser.save()
-    res.json(newUser).status(200)
-  }catch(e){
-    res.json({error:`error processing on /user post route ${e}`}).status(500)
-  }
-})
 
 db.once('connected', () => {
   console.log('DB connected')
