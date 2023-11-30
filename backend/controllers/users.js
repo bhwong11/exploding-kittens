5e4e97d18c0943b89bd4d217d7c977d5b7868c7d
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2023-11-30 14:46:13 -0800

    added email/password to user schema, installed bcrypt dependency, implemented salt/hashing for passwords

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt'
 import User from "../models/User.js";
 
 const all = async (req,res)=>{
@@ -11,14 +12,20 @@ const all = async (req,res)=>{
 
 const create = async (req,res)=>{
   try{
-    const username = req.body.username
+    const { username, password, email } = req.body  
+    console.log(username, password) 
     const existingUser = await User.findOne({username})
 
     if(existingUser) {
       return res.json({error:`user with ${username} already exist`}).status(400)
     }
 
-    const newUser = new User({username})
+    const salt = await bcrypt.genSalt(12)
+    console.log('salt:', salt)
+    const hash = await bcrypt.hash(password, salt)
+    console.log('hash:', hash)
+
+    const newUser = new User({ username, email, password: hash })
     await newUser.save()
     res.json(newUser).status(200)
   }catch(e){
--- backend/models/User.js
+++ backend/models/User.js
@@ -4,11 +4,14 @@ const { Schema } = mongoose
 
 const userSchema = new Schema({
   username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
   wins: { type: Number, required: true, default:0 },
   rooms:[
     {type: Schema.Types.ObjectId, ref: 'Room'}
   ]
   //need to add game state data to save
+  // also need to add pw 
 },{ timestamps: true });
 
 const User = mongoose.model('User', userSchema);
