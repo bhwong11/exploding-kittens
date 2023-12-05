98f6d8d4edae524a526d0e6ad6b96ae89f564079
Courtney Kakebeen <84427378+LAsoul88@users.noreply.github.com>
2023-12-04 16:10:05 -0800

    Update index.js
    reinstated limit()

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(express.json())
 app.get('/', async (req, res) => {
   let collection = db.collection("posts");
   let results = await collection.find({})
-    // .limit(50)
+    .limit(50)
     // not sure .limit() works on .find() 
     // I was getting an error so it'll be commented out for now
     .toArray();
