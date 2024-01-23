65bfd1dcd8f7785509e235eaf0a488b8c5051cae
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-01-22 17:54:02 -0800

    setting up navbar

--- exploding-kittens-frontend/src/app/(components)/navbar.tsx
+++ exploding-kittens-frontend/src/app/(components)/navbar.tsx
@@ -1,5 +1,28 @@
-export const Navbar = ()=>{
-  <>
-    NAVBAR
-  </>
+'use client'
+import { usePlayerContext } from '@/context/players'
+
+export const Navbar = () => {
+  const { currentPlayer } = usePlayerContext() || {}
+
+  return (
+    <nav className="fixed w-full h-10 flex justify-between bg-slate-500 p-2">
+      <div className="img-container">
+        <img /> {/* Icon for site? */}
+      </div>
+      <div className="flex justify-end gap-5">
+        <a
+          href=""
+          className=""
+        >
+          {currentPlayer?.username || 'Login'}
+        </a>
+        <a
+          href=""
+          className=""
+        >
+          Logout
+        </a>
+      </div>
+    </nav>
+  )
 }
