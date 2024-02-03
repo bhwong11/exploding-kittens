8f0040df4b248c55f9db5793d439ccd3e67f22ed
Bryant Wong <bryanthwong@gmail.com>
2024-02-03 16:55:46 -0500

    added dec visual component

---
+++ exploding-kittens-frontend/src/app/[roomNumber]/CardPile.tsx
@@ -0,0 +1,41 @@
+type CardPileProps = {
+  cards: Card[]
+  //in rem
+  width?: number
+  height?: number
+  cardOffset?: number
+  cardsPerVisualCard?: number,
+  title: string
+}
+const CardPile = ({
+  cards,
+  //in rem
+  width = 5,
+  height = 7,
+  cardOffset = 0.3,
+  cardsPerVisualCard = 5,
+  title = ''
+}:CardPileProps)=>{
+  const cardsDisplayed = Math.floor((cards?.length || 0)/cardsPerVisualCard)
+  return (
+    <div>
+      <p>{title}</p>
+      <div style={{
+        height:`${height+cardOffset*cardsDisplayed}rem`
+      }}>
+        {Array.from(Array(cardsDisplayed).keys()).map((idx)=>(
+          <div className="w-[5rem] border border-black bg-gray-500 shadow-lg relative" style={{
+            height: `${height}rem`,
+            width:`${width}rem`,
+            top: `-${idx===0?0:idx*(height-cardOffset)}rem`,
+            left:`${idx===0?0:idx*cardOffset}rem`
+          }}>
+            {idx===cardsDisplayed-1 && `cards: ${cards.length}`}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default CardPile
